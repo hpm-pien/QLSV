7e4052b6f3a991edf7083ce75bfe085ef089fd26
Pien Tran <80038065+hpm-pien@users.noreply.github.com>
2022-04-18 12:22:20 +0700

    QLSV (update)

---
+++ src/PE01/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace PE01
+{
+    partial class FormPE01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtClassCode = new System.Windows.Forms.TextBox();
+            this.txtSubjectCode = new System.Windows.Forms.TextBox();
+            this.dateDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.svinfoDataGridView = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnFillData = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.svinfoDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Full Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Date Of Birth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Class Code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(68, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Subject Code";
+            // 
+            // txtId
+            // 
+            this.txtId.AcceptsTab = true;
+            this.txtId.Location = new System.Drawing.Point(188, 26);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(178, 22);
+            this.txtId.TabIndex = 5;
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(188, 61);
+            this.txtFullName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(178, 22);
+            this.txtFullName.TabIndex = 6;
+            // 
+            // txtClassCode
+            // 
+            this.txtClassCode.Location = new System.Drawing.Point(188, 129);
+            this.txtClassCode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtClassCode.Name = "txtClassCode";
+            this.txtClassCode.Size = new System.Drawing.Size(178, 22);
+            this.txtClassCode.TabIndex = 7;
+            // 
+            // txtSubjectCode
+            // 
+            this.txtSubjectCode.Location = new System.Drawing.Point(188, 162);
+            this.txtSubjectCode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSubjectCode.Name = "txtSubjectCode";
+            this.txtSubjectCode.Size = new System.Drawing.Size(178, 22);
+            this.txtSubjectCode.TabIndex = 8;
+            // 
+            // dateDateTimePicker
+            // 
+            this.dateDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateDateTimePicker.Location = new System.Drawing.Point(188, 96);
+            this.dateDateTimePicker.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateDateTimePicker.Name = "dateDateTimePicker";
+            this.dateDateTimePicker.Size = new System.Drawing.Size(178, 22);
+            this.dateDateTimePicker.TabIndex = 9;
+            // 
+            // svinfoDataGridView
+            // 
+            this.svinfoDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.svinfoDataGridView.Location = new System.Drawing.Point(52, 220);
+            this.svinfoDataGridView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.svinfoDataGridView.Name = "svinfoDataGridView";
+            this.svinfoDataGridView.RowHeadersWidth = 51;
+            this.svinfoDataGridView.RowTemplate.Height = 28;
+            this.svinfoDataGridView.Size = new System.Drawing.Size(604, 212);
+            this.svinfoDataGridView.TabIndex = 10;
+            this.svinfoDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.svinfoDataGridView_CellClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(441, 84);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(96, 33);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnFillData
+            // 
+            this.btnFillData.Location = new System.Drawing.Point(441, 27);
+            this.btnFillData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnFillData.Name = "btnFillData";
+            this.btnFillData.Size = new System.Drawing.Size(198, 35);
+            this.btnFillData.TabIndex = 12;
+            this.btnFillData.Text = "Fill Data";
+            this.btnFillData.UseVisualStyleBackColor = true;
+            this.btnFillData.Click += new System.EventHandler(this.btnFillData_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(553, 84);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(84, 33);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(441, 145);
+            this.btnNew.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(80, 38);
+            this.btnNew.TabIndex = 14;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(553, 145);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 38);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // FormPE01
+            // 
+            this.AcceptButton = this.btnFillData;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 457);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnFillData);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.svinfoDataGridView);
+            this.Controls.Add(this.dateDateTimePicker);
+            this.Controls.Add(this.txtSubjectCode);
+            this.Controls.Add(this.txtClassCode);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormPE01";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PE01";
+            ((System.ComponentModel.ISupportInitialize)(this.svinfoDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtClassCode;
+        private System.Windows.Forms.TextBox txtSubjectCode;
+        private System.Windows.Forms.DateTimePicker dateDateTimePicker;
+        private System.Windows.Forms.DataGridView svinfoDataGridView;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnFillData;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnExit;
+
+      
+    }
+}
+
---
+++ src/PE01/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace PE01
+{
+    public partial class FormPE01 : Form
+    {
+        
+        public FormPE01()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cnn;
+        SqlCommand cmd;
+        String sql = "Data Source=HP-Pien;Initial Catalog=SV;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        private void btnFillData_Click(object sender, EventArgs e)
+        {
+            cnn = new SqlConnection(sql);
+            cnn.Open();
+            loadData();
+        }
+        public void loadData()
+        {
+            cmd = cnn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM SVINFO";
+            adapter.SelectCommand = cmd;
+            table.Clear();
+            adapter.Fill(table);
+            svinfoDataGridView.DataSource = table;
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtId.Enabled = true;
+            txtId.Clear();
+            txtFullName.Clear();
+            dateDateTimePicker.ResetText();
+            txtClassCode.Clear();
+            txtSubjectCode.Clear();
+            btnInsert.Enabled = true;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            cnn = new SqlConnection(sql);
+            string sqlAdd = "insert into SVINFO(Id, FullName, DateOfBirth, ClassCode, SubjectCode) values (@Id, @Name, @DateOfBirth, @ClassCode, @SubjectCode)";
+            SqlCommand cmd1 = new SqlCommand(sqlAdd, cnn);
+            cmd1.Parameters.AddWithValue("@Id", txtId.Text);
+            cmd1.Parameters.AddWithValue("@Name", txtFullName.Text);
+            cmd1.Parameters.AddWithValue("@DateOfBirth", dateDateTimePicker.Value);
+            cmd1.Parameters.AddWithValue("@ClassCode", txtClassCode.Text);
+            cmd1.Parameters.AddWithValue("@SubjectCode", txtSubjectCode.Text);
+            cnn.Open();
+            cmd1.ExecuteNonQuery();
+            cnn.Close();
+            loadData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            cnn = new SqlConnection(sql);
+            String sqlUpdate = "Update SVINFO Set FullName = @Name, DateOfBirth = @DateOfBirth, ClassCode = @ClassCode, SubjectCode = @SubjectCode Where Id = '" + txtId.Text + "'";
+            SqlCommand cmd2 = new SqlCommand(sqlUpdate, cnn);
+            cmd2.Parameters.AddWithValue("@Name", txtFullName.Text);
+            cmd2.Parameters.AddWithValue("@DateOfBirth", dateDateTimePicker.Value);
+            cmd2.Parameters.AddWithValue("@ClassCode", txtClassCode.Text);
+            cmd2.Parameters.AddWithValue("@SubjectCode", txtSubjectCode.Text);
+            cnn.Open();
+            cmd2.ExecuteNonQuery();
+            cnn.Close();
+            loadData();
+            btnInsert.Enabled = true;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult h = MessageBox.Show("DO YOU WANT TO EXIT?", "ERROR", MessageBoxButtons.OKCancel);
+            if(h == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void svinfoDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = this.svinfoDataGridView.Rows[e.RowIndex];
+            txtId.Text = row.Cells[0].Value.ToString();
+            txtFullName.Text = row.Cells[1].Value.ToString();
+            dateDateTimePicker.Text = row.Cells[2].Value.ToString();
+            txtClassCode.Text = row.Cells[3].Value.ToString();
+            txtSubjectCode.Text = row.Cells[4].Value.ToString();
+
+            txtId.Enabled = false;
+            btnInsert.Enabled = false;
+        }
+    }
+}
